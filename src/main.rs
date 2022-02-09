4695f21af6e0f339ed7d9b26a24d014c0cdc3654
Andrew Hao <andrewhao@gmail.com>
2022-02-09 14:57:19 -0800

    WIP commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use clap::Parser;
-use std::convert::Infallible;
-use hyper::{Body, Request, Response, Server};
 use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
+use hyper::Client;
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::result::Result;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -17,27 +19,43 @@ struct Proxy {
     port: u16,
 }
 
+async fn proxy_inner(
+    req: Request<Body>,
+    client: Client<hyper::client::HttpConnector>,
+) -> Result<Response<Body>, hyper::Error> {
+    // Await the response...
+
+    let (parts, body) = req.into_parts();
+    let path: String = parts.uri.path().parse::<String>().unwrap_or_default();
+    let host: &str = parts.uri.host().unwrap();
+    let scheme: &str = parts.uri.scheme_str().unwrap();
+    let headers = parts.headers;
+
+    // let uri = ("https://www.wejoinin.com/" + req.uri().path()).parse?;
+    let uri = "http://localhost:3000".parse().unwrap();
+    client.get(uri).await
+    // Ok(Response::builder().status(200).body(Body::empty()).unwrap())
+}
+
 impl Proxy {
     pub fn new(port: u16) -> Self {
-        Proxy {
-            port,
-        }
+        Proxy { port }
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let addr = ([0, 0, 0, 0], self.port).into();
 
         println!("Running proxy on: {}", &addr);
+        let client = Client::new();
 
         let make_svc = make_service_fn(move |socket: &AddrStream| {
             let remote_addr = socket.remote_addr();
+            let client = client.clone();
             println!("Handling connection for IP: {}", &remote_addr);
 
             async move {
-                Ok::<_, Infallible>(service_fn(move |_: Request<Body>| async move {
-                    Ok::<_, Infallible>(
-                        Response::new(Body::from(format!("Hello, {}!", remote_addr)))
-                    )
+                Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                    proxy_inner(req, client.clone())
                 }))
             }
         });
